132c70902b1cf3d8c95289688b2ad7f8c56a7f43
David-Ake <55287581+David-Ake@users.noreply.github.com>
2020-01-15 11:56:28 -0500

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,7 +1,17 @@
+var beginning = 20;
+var limit = -20;
+
+/*while(beginning < limit)
+  {
+    document.write("Current Count: " + beginning)
+    beginning++;
+  }*/
+
 while(beginning >= limit)
   {
     document.write("Current Count: " + beginning + "<br />")
     beginning++;
     beginning = beginning -2;
   }
+  
   
